fb03f675feafbf1f92978fd0448db79def582cd8
phamquanganh <phamquanganh705@gmail.com>
2022-09-03 22:26:01 +0700

    add simulator

---
+++ simulator/build/CMakeFiles/ranc_sim.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/phamquanganh/RANC/simulator/src/core.cpp" "/home/phamquanganh/RANC/simulator/build/CMakeFiles/ranc_sim.dir/src/core.cpp.o"
+  "/home/phamquanganh/RANC/simulator/src/corecontroller.cpp" "/home/phamquanganh/RANC/simulator/build/CMakeFiles/ranc_sim.dir/src/corecontroller.cpp.o"
+  "/home/phamquanganh/RANC/simulator/src/csramrow.cpp" "/home/phamquanganh/RANC/simulator/build/CMakeFiles/ranc_sim.dir/src/csramrow.cpp.o"
+  "/home/phamquanganh/RANC/simulator/src/main.cpp" "/home/phamquanganh/RANC/simulator/build/CMakeFiles/ranc_sim.dir/src/main.cpp.o"
+  "/home/phamquanganh/RANC/simulator/src/neuronblock.cpp" "/home/phamquanganh/RANC/simulator/build/CMakeFiles/ranc_sim.dir/src/neuronblock.cpp.o"
+  "/home/phamquanganh/RANC/simulator/src/outputbus.cpp" "/home/phamquanganh/RANC/simulator/build/CMakeFiles/ranc_sim.dir/src/outputbus.cpp.o"
+  "/home/phamquanganh/RANC/simulator/src/packet.cpp" "/home/phamquanganh/RANC/simulator/build/CMakeFiles/ranc_sim.dir/src/packet.cpp.o"
+  "/home/phamquanganh/RANC/simulator/src/rancgrid.cpp" "/home/phamquanganh/RANC/simulator/build/CMakeFiles/ranc_sim.dir/src/rancgrid.cpp.o"
+  "/home/phamquanganh/RANC/simulator/src/router.cpp" "/home/phamquanganh/RANC/simulator/build/CMakeFiles/ranc_sim.dir/src/router.cpp.o"
+  "/home/phamquanganh/RANC/simulator/src/scheduler.cpp" "/home/phamquanganh/RANC/simulator/build/CMakeFiles/ranc_sim.dir/src/scheduler.cpp.o"
+  "/home/phamquanganh/RANC/simulator/src/schedulersram.cpp" "/home/phamquanganh/RANC/simulator/build/CMakeFiles/ranc_sim.dir/src/schedulersram.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../extern"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ simulator/build/CMakeFiles/ranc_sim.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ranc_sim.dir/src/core.cpp.o"
+  "CMakeFiles/ranc_sim.dir/src/corecontroller.cpp.o"
+  "CMakeFiles/ranc_sim.dir/src/csramrow.cpp.o"
+  "CMakeFiles/ranc_sim.dir/src/main.cpp.o"
+  "CMakeFiles/ranc_sim.dir/src/neuronblock.cpp.o"
+  "CMakeFiles/ranc_sim.dir/src/outputbus.cpp.o"
+  "CMakeFiles/ranc_sim.dir/src/packet.cpp.o"
+  "CMakeFiles/ranc_sim.dir/src/rancgrid.cpp.o"
+  "CMakeFiles/ranc_sim.dir/src/router.cpp.o"
+  "CMakeFiles/ranc_sim.dir/src/scheduler.cpp.o"
+  "CMakeFiles/ranc_sim.dir/src/schedulersram.cpp.o"
+  "ranc_sim"
+  "ranc_sim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ranc_sim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
